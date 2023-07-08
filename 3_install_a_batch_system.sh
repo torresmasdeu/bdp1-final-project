20cbbe90607a4a34e9b235dc123f8586ca5c7353
giacomoorsini <133371083+giacomoorsini@users.noreply.github.com>
2023-07-08 17:10:45 +0200

    Update 3_install_a_batch_system.sh G3

--- 3_install_a_batch_system.sh
+++ 3_install_a_batch_system.sh
@@ -28,11 +28,11 @@ vi /etc/condor/condor_config
 #HOSTALLOW\_READ = * 
 #HOSTALLOW\_WRITE = * 
 HOSTALLOW\_ADMINISTRATOR = *
-                                                                  #Rememebr that the security Group must allow tcp for ports 0 - 65535 from the same security group, i.e.: All TCP TCP 0 - 65535 sg-008742ba0467986fe (aws_condor) Security group must allow ping
+                                                                  #Remember that the security Group must allow tcp for ports 0 - 65535 from the same security group, i.e.: All TCP TCP 0 - 65535 sg-008742ba0467986fe (aws_condor) Security group must allow ping
                                                                   #...from the same security group, i.e.: All ICMP-IPv4 All N/A sg-008742ba0467986fe (aws_condor) Security group must allow ssh on port 22 from everywhere as ususal
 ##############CONDOR FINAL CONFIGURATION ON THE MASTER AND WORKER(S) NODES##############
 systemctl status condor
 systemctl start condor                                            #Start condor...
-systemctl enable condor                                                                   #...and check that it works
+systemctl enable condor                                           #...and check that it works
 systemctl status condor
 ps -aux | grep condor
