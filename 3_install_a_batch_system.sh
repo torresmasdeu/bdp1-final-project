9cc1bc60885350033070f868260f6e36905c52d1
giacomoorsini <133371083+giacomoorsini@users.noreply.github.com>
2023-07-08 12:53:53 +0200

    Update 3_install_a_batch_system.sh G1

--- 3_install_a_batch_system.sh
+++ 3_install_a_batch_system.sh
@@ -1,33 +1,34 @@
 #################INSTALL DEPENDENCIES ON THE MASTER AND WORKER(S) NODES#################
-yum install wget -y 
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install wget -y                                                                       #install wget if not already installed
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm             
 yum localinstall epel-release-latest-7.noarch.rpm -y
 yum clean all
 
-############INSTALL CONDOR REPOS AND PACKAGES ON THE MASTER AND WORKER NODES############
+############INSTALL CONDOR REPOS AND PACKAGES ON THE MASTER AND WORKER(S) NODES############
 wget http://research.cs.wisc.edu/htcondor/yum/repo.d/htcondor-stable-rhel7.repo
 cp htcondor-stable-rhel7.repo /etc/yum.repos.d/
 yum install condor-all -y
 
 #####################CONDOR BASIC CONFIGURATION ON THE MASTER NODE######################
-
-On the master node, the /etc/condor/condor\_config was modified as such:
-
-#CONDOR\_HOST = master Private IP address
-
-#DAEMON\_LIST = COLLECTOR, MASTER, NEGOTIATOR, STARTD, SCHEDD
-
+cd                                                                                        #In both master and worker(s) we have to modify the condor_fig file to configure our nodes in HTCondor system
+vi /etc/condor/condor_config
+                                                                                          #In the config_file of the master node add at the end the following lines
+#CONDOR\_HOST = master Private IP address                                                 #Specify the private IP of the master
+#DAEMON\_LIST = COLLECTOR, MASTER, NEGOTIATOR, STARTD, SCHEDD                             #The master node is configured as collector, master, negotiator, executer and scheduler
 #HOSTALLOW\_READ = * HOSTALLOW\_WRITE = * HOSTALLOW\_ADMINISTRATOR =
 
 ####################CONDOR BASIC CONFIGURATION ON THE WORKER NODE(S)####################
-on the worker nodes, the same file was modified as such:
-#CONDOR\_HOST = worker Private IP address
-#on the nodes DAEMON\_LIST = MASTER, STARTD
+cd
+vi /etc/condor/condor_config
+                                                                                          #In the config_file of the worker node add at the end the following lines
+#CONDOR\_HOST = worker Private IP address                                                 #Specify the private IP of the master
+#on the nodes DAEMON\_LIST = MASTER, STARTD                                               #The worker node is configured as master and executer
 #HOSTALLOW\_READ = * HOSTALLOW\_WRITE = * HOSTALLOW\_ADMINISTRATOR = *
-
+                                                                                          #Rememebr that the security Group must allow tcp for ports 0 - 65535 from the same security group, i.e.: All TCP TCP 0 - 65535 sg-008742ba0467986fe (aws_condor) Security group must allow ping
+                                                                                          #...from the same security group, i.e.: All ICMP-IPv4 All N/A sg-008742ba0467986fe (aws_condor) Security group must allow ssh on port 22 from everywhere as ususal
 ##############CONDOR FINAL CONFIGURATION ON THE MASTER AND WORKER(S) NODES##############
 systemctl status condor
-systemctl start condor
-systemctl enable condor
+systemctl start condor                                                                    #Start condor...
+systemctl enable condor                                                                   #...and check that it works
 systemctl status condor
 ps -aux | grep condor
