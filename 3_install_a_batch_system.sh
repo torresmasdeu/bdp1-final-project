93c577cd764557380c0111a0ba84a1b299de7117
giacomoorsini <133371083+giacomoorsini@users.noreply.github.com>
2023-07-08 12:55:12 +0200

    Update 3_install_a_batch_system.sh G1.2

--- 3_install_a_batch_system.sh
+++ 3_install_a_batch_system.sh
@@ -15,7 +15,9 @@ vi /etc/condor/condor_config
                                                                                           #In the config_file of the master node add at the end the following lines
 #CONDOR\_HOST = master Private IP address                                                 #Specify the private IP of the master
 #DAEMON\_LIST = COLLECTOR, MASTER, NEGOTIATOR, STARTD, SCHEDD                             #The master node is configured as collector, master, negotiator, executer and scheduler
-#HOSTALLOW\_READ = * HOSTALLOW\_WRITE = * HOSTALLOW\_ADMINISTRATOR =
+#HOSTALLOW\_READ = * 
+#HOSTALLOW\_WRITE = * 
+#HOSTALLOW\_ADMINISTRATOR = *
 
 ####################CONDOR BASIC CONFIGURATION ON THE WORKER NODE(S)####################
 cd
@@ -23,7 +25,9 @@ vi /etc/condor/condor_config
                                                                                           #In the config_file of the worker node add at the end the following lines
 #CONDOR\_HOST = worker Private IP address                                                 #Specify the private IP of the master
 #on the nodes DAEMON\_LIST = MASTER, STARTD                                               #The worker node is configured as master and executer
-#HOSTALLOW\_READ = * HOSTALLOW\_WRITE = * HOSTALLOW\_ADMINISTRATOR = *
+#HOSTALLOW\_READ = * 
+#HOSTALLOW\_WRITE = * 
+HOSTALLOW\_ADMINISTRATOR = *
                                                                                           #Rememebr that the security Group must allow tcp for ports 0 - 65535 from the same security group, i.e.: All TCP TCP 0 - 65535 sg-008742ba0467986fe (aws_condor) Security group must allow ping
                                                                                           #...from the same security group, i.e.: All ICMP-IPv4 All N/A sg-008742ba0467986fe (aws_condor) Security group must allow ssh on port 22 from everywhere as ususal
 ##############CONDOR FINAL CONFIGURATION ON THE MASTER AND WORKER(S) NODES##############
